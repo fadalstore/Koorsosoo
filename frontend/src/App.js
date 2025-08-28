b956813a7069f04cf091c31a459e5541078b7385
fadalstore <fadalstore7@gmail.com>
2025-08-28 17:00:10 +0300

    Dhameystir React website-ka aasaasiga ah

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h1>Ku soo dhawoow Koorsosoo!</h1>
+      <p>Website-kan waxaa lagu sameeyay React.</p>
+    </div>
+  );
+}
+
+export default App;
